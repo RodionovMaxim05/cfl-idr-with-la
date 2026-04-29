0ec8553d2ab64cf26533e80a90c8d66da5416d1c
Maxim Rodionov <rodionov.maxim05@gmail.com>
2026-04-29 17:57:22 +0300

    feat: add a function that removes unnecessary edges (remove_not_path)

---
+++ src/graph/remove_not_path.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "GraphBLAS.h"
+#include "approximation/mr_graph.h"
+
+typedef struct {
+	GrB_Index n_scc;
+	GrB_Index *scc_ids;
+	GrB_Matrix scc_reach;
+} SccResult;
+
+GrB_Info compute_sccs(const MRGraph *graph, SccResult *out, char *msg);
+
+void scc_result_free(SccResult *r);
+
+GrB_Info remove_not_path(const MRGraph *graph, GrB_Matrix over_approx, MRGraph *out,
+						 char *msg);
